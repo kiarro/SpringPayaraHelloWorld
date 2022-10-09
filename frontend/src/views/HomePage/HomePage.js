113bba2db3e9afa5f21b1f24aa55aa45fa94a7ba
Anna Shiyan <an.sshiyan@gmail.com>
2022-10-09 10:58:02 +0300

    add necessary view elements
    the ugliest version

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import './HomePage.css';
 import DragonBase from './../../components/Tables/DragonBase'
 import HeaderButton from './../../components/Buttons/HeaderButton';
 import BaseInput from './../../components/Inputs/BaseInput';
+import filterFactory, { textFilter,selectFilter, numberFilter } from 'react-bootstrap-table2-filter';
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,15 +13,75 @@ import {
   useParams,
 } from 'react-router-dom';
 
+const selectTypes = {
+  WATER: 'WATER',
+  UNDERGROUND:'UNDERGROUND',
+  AIR:'AIR',
+  FIRE:'FIRE'
+};
 
-const headers = ['ID', 'ИМЯ', 'ВОЗРАСТ', 'ВЕС','ХАРАКТЕР','ТИП','Х','У','ГЛУБИНА ПЕЩЕРЫ','КОЛИЧЕСТВО СОКРОВИЩ'];
+const selectCharacters = {
+  CUNNING: 'CUNNING',
+  WISE: 'WISE',
+  CHAOTIC: 'CHAOTIC',
+  CHAOTIC_EVIL: 'CHAOTIC_EVIL',
+  FICKLE: 'FICKLE'
+};
+
+const headers = [{
+  dataField: 'id',
+  text:'ID',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'name',
+  text:'ИМЯ',
+  sort: true,
+  filter: textFilter()},{
+  dataField:'age',
+  text:'ВОЗРАСТ',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'weight',
+  text:'ВЕС',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'character',
+  text:'ХАРАКТЕР',
+  sort: true,
+  filter: selectFilter({
+    options: selectCharacters
+  })},{
+  dataField:'type',
+  text:'ТИП',
+  sort: true,
+  filter: selectFilter({
+    options: selectTypes
+  })},{
+  dataField:'x',
+  text:'Х',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'y',
+  text:'У',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'cave',
+  text:'ГЛУБИНА ПЕЩЕРЫ',
+  sort: true,
+  filter: numberFilter()},{
+  dataField:'treasure',
+  text:'КОЛИЧЕСТВО СОКРОВИЩ',
+  sort: true,
+  filter: numberFilter()}
+];
+
 
 const rows = [
-  ['1', 'Балерион', '208', '10','Хаос','Огненный','12','4','1400','157'],
-  ['2', 'Вхагар', '181', '18','Хаос','Огненный','12','4','1400','157'],
-  ['3', 'Мераксес','181', '18','Хаос','Огненный','12','4','1400','157'],
-  ['4', 'Мераксес','181', '18','Хаос','Огненный','12','4','1400','157'],
-  ['5', 'Мераксес','181', '18','Хаос','Огненный','12','4','1400','157'],
+{id:'1',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'2',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'3',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'4',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'5',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'}
 ];
 
 function HomePage()
