0592727fc4bc95b507ddd06270fe78c87926f6ae
Anna Shiyan <an.sshiyan@gmail.com>
2022-10-16 11:34:00 +0300

    test acceptable view

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -36,7 +36,13 @@ const headers = [{
   dataField:'name',
   text:'ИМЯ',
   sort: true,
-  filter: textFilter()},{
+  filter: textFilter({
+    placeholder: 'Введите имя',  // custom the input placeholder
+    //style: { backgroundColor: '#6b63b5' },
+    className: 'my-custom-text-filter' // custom classname on input
+    
+  })
+},{
   dataField:'age',
   text:'ВОЗРАСТ',
   sort: true,
@@ -81,7 +87,17 @@ const rows = [
 {id:'2',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
 {id:'3',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
 {id:'4',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'5',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'}
+{id:'5',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'6',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'7',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'8',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'9',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'10',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'11',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'12',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'13',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'14',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
+{id:'15',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'}
 ];
 
 function HomePage()
@@ -92,6 +108,8 @@ function HomePage()
     <DragonBase rows={rows} headers={headers} rowHeaders page="1 из я хуй знает скольки" />
     </main>
     <footer>
+    <ul>
+      <li className='OperationItem'>
     <ul className='NeededOperations'>
     <li>
     <BaseInput 
@@ -101,6 +119,8 @@ function HomePage()
     </li>
     <li><Link to="/returnarray"><HeaderButton name="Вернуть"/></Link></li>
     </ul>
+    </li>
+      <li className='OperationItem'>
     <ul className='NeededOperations'> 
     <li>
     <BaseInput 
@@ -110,6 +130,8 @@ function HomePage()
     </li>
     <li><Link to='/charactercount'> <HeaderButton name="Вернуть"/></Link> </li>
     </ul>
+    </li>
+    <li className='OperationItem'>
     <ul className='NeededOperations'>
     <li>
     <BaseInput 
@@ -119,6 +141,9 @@ function HomePage()
     </li>
     <li><Link to='/typecount'> <HeaderButton name="Вернуть"/> </Link></li>
     </ul> 
+    </li>   
+    </ul>
+    
     </footer>
     </div>
   );
