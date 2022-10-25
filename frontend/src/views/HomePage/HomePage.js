e9c34c0961abe54110eb2589612f41d68552d742
Юрий <38157715+kiarro@users.noreply.github.com>
2022-10-25 14:59:42 +0300

    Editing added

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import HeaderButton from './../../components/Buttons/HeaderButton';
 import BaseInput from './../../components/Inputs/BaseInput';
 import Select from './../../components/Inputs/Select';
 
-import filterFactory, { textFilter,selectFilter, numberFilter, Comparator } from 'react-bootstrap-table2-filter';
+import filterFactory, { textFilter, selectFilter, numberFilter, Comparator } from 'react-bootstrap-table2-filter';
 import {
   BrowserRouter as Router,
   Routes,
@@ -22,9 +22,9 @@ import {
 
 const selectTypes = {
   WATER: 'WATER',
-  UNDERGROUND:'UNDERGROUND',
-  AIR:'AIR',
-  FIRE:'FIRE'
+  UNDERGROUND: 'UNDERGROUND',
+  AIR: 'AIR',
+  FIRE: 'FIRE'
 };
 
 const selectCharacters = {
@@ -35,112 +35,26 @@ const selectCharacters = {
   FICKLE: 'FICKLE'
 };
 
-const headers = [{
-  dataField: 'id',
-  text:'ID',
-  sort: true,
-  filter: numberFilter(
-    {
-    placeholder: 'Введите id'
-    }
-  )},{
-  dataField:'name',
-  text:'ИМЯ',
-  sort: true,
-  filter: textFilter({
-    placeholder: 'Введите имя',  // custom the input placeholder
-    //style: { backgroundColor: '#6b63b5' },
-    className: 'my-custom-text-filter' // custom classname on input
-    
-  })
-},{
-  dataField:'age',
-  text:'ВОЗРАСТ',
-  sort: true,
-  filter: numberFilter(
-    {
-      placeholder: 'Введите возраст'
-    }
-  )},{
-  dataField:'weight',
-  text:'ВЕС',
-  sort: true,
-  filter: numberFilter(
-    {
-      style: { weight: 10 },
-      placeholder: 'Введите вес'
-    }
-  )},{
-  dataField:'character',
-  text:'ХАРАКТЕР',
-  sort: true,
-  filter: selectFilter({
-    options: selectCharacters,
-    placeholder: 'Выберите характер'
-  })},{
-  dataField:'type',
-  text:'ТИП',
-  sort: true,
-  filter: selectFilter({
-    options: selectTypes,
-    placeholder: 'Выберите тип'
-  })},{
-  dataField:'coordinates.x',
-  text:'Х',
-  sort: true,
-  filter: numberFilter(
-    {
-      placeholder: 'Введите Х'
-    }
-  )},{
-  dataField:'coordinates.y',
-  text:'У',
-  sort: true,
-  filter: numberFilter(
-    {
-      placeholder: 'Введите У'
-    }
-  )},{
-  dataField:'cave.depth',
-  text:'ГЛУБИНА ПЕЩЕРЫ',
-  sort: true,
-  filter: numberFilter(
-    {
-      placeholder: 'Введите глубину пещеры'
-    }
-  )},{
-  dataField:'cave.numberOfTreasures',
-  text:'КОЛИЧЕСТВО СОКРОВИЩ',
-  sort: true,
-  filter: numberFilter(
-    {
-      placeholder: 'Введите количество сокровищ'
-    }
-  )}
-];
-
-
 const rows = [
-{id:'1',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'2',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'3',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'4',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'5',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'6',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'7',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'8',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'9',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'10',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'11',name: 'Балерион',age: '208',weight: '10',character:'CHAOTIC_EVIL',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'12',name: 'Вхагар',age: '181',weight: '18',character:'FICKLE',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'13',name: 'Мераксес',age:'181',weight: '18',character:'CUNNING',type:'FIRE',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'14',name: 'Мераксес',age:'181',weight: '18',character:'WISE',type:'AIR',x:'12',y:'4',cave:'1400',treasure:'157'},
-{id:'15',name: 'Мераксес',age:'181',weight: '18',character:'CHAOTIC',type:'WATER',x:'12',y:'4',cave:'1400',treasure:'157'}
+  { id: '1', name: 'Балерион', age: '208', weight: '10', character: 'CHAOTIC_EVIL', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '2', name: 'Вхагар', age: '181', weight: '18', character: 'FICKLE', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '3', name: 'Мераксес', age: '181', weight: '18', character: 'CUNNING', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '4', name: 'Мераксес', age: '181', weight: '18', character: 'WISE', type: 'AIR', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '5', name: 'Мераксес', age: '181', weight: '18', character: 'CHAOTIC', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '6', name: 'Балерион', age: '208', weight: '10', character: 'CHAOTIC_EVIL', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '7', name: 'Вхагар', age: '181', weight: '18', character: 'FICKLE', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '8', name: 'Мераксес', age: '181', weight: '18', character: 'CUNNING', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '9', name: 'Мераксес', age: '181', weight: '18', character: 'WISE', type: 'AIR', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '10', name: 'Мераксес', age: '181', weight: '18', character: 'CHAOTIC', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '11', name: 'Балерион', age: '208', weight: '10', character: 'CHAOTIC_EVIL', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '12', name: 'Вхагар', age: '181', weight: '18', character: 'FICKLE', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '13', name: 'Мераксес', age: '181', weight: '18', character: 'CUNNING', type: 'FIRE', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '14', name: 'Мераксес', age: '181', weight: '18', character: 'WISE', type: 'AIR', x: '12', y: '4', cave: '1400', treasure: '157' },
+  { id: '15', name: 'Мераксес', age: '181', weight: '18', character: 'CHAOTIC', type: 'WATER', x: '12', y: '4', cave: '1400', treasure: '157' }
 ];
 
 
-function HomePage()
-{
+function HomePage() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [dragons, setDragons] = useState([]);
@@ -171,52 +85,52 @@ function HomePage()
   } else {
     return (
       <div>
-      <main>
-      <DragonBase rows={dragons} headers={headers} rowHeaders page="1 из я хуй знает скольки" />
-      </main>
-      <footer>
-      <ul>
-        <li className='OperationItem'>
-      <ul className='NeededOperations'>
-      <li>
-      <BaseInput 
-      name="Массив драконов, имя которых начинается с:" 
-      enter="Введите подстроку имени"
-      value={namestarts} onChange={(e) => setNamestarts(e.target.value)}
-      />
-      </li>
-      <li><Link to={"/namestarts?name="+namestarts}><HeaderButton name="Вернуть"/></Link></li>
-      </ul>
-      </li>
-        <li className='OperationItem'>
-      <ul className='NeededOperations'> 
-      <li>
-      <Select 
-      name="Количество драконов, характер которых больше:"
-      enter="Введите характер"
-      value={charactermore} onChange={(e) => setCharactermore(e.target.value)} 
-      options={["CUNNING", "WISE", "CHAOTIC", "CHAOTIC_EVIL", "FICKLE"]}>
-      </Select>
-      </li>
-      <li><Link to={'/charactermore?character='+charactermore}> <HeaderButton name="Вернуть"/></Link> </li>
-      </ul>
-      </li>
-      <li className='OperationItem'>
-      <ul className='NeededOperations'>
-      <li>
-      <Select 
-      name="Количество драконов, тип которых меньше:"
-      enter="Введите тип"
-      value={typeless} onChange={(e) => setTypeless(e.target.value)} 
-      options={["WATER", "UNDERGROUND", "AIR", "FIRE"]}>
-      </Select>
-      </li>
-      <li><Link to={'/typeless?type='+typeless}> <HeaderButton name="Вернуть"/> </Link></li>
-      </ul> 
-      </li>   
-      </ul>
-      
-      </footer>
+        <main>
+          <DragonBase rows={dragons} rowHeaders page="1 из я хуй знает скольки" />
+        </main>
+        <footer>
+          <ul>
+            <li className='OperationItem'>
+              <ul className='NeededOperations'>
+                <li>
+                  <BaseInput
+                    name="Массив драконов, имя которых начинается с:"
+                    enter="Введите подстроку имени"
+                    value={namestarts} onChange={(e) => setNamestarts(e.target.value)}
+                  />
+                </li>
+                <li><Link to={"/namestarts?name=" + namestarts}><HeaderButton name="Вернуть" /></Link></li>
+              </ul>
+            </li>
+            <li className='OperationItem'>
+              <ul className='NeededOperations'>
+                <li>
+                  <Select
+                    name="Количество драконов, характер которых больше:"
+                    enter="Введите характер"
+                    value={charactermore} onChange={(e) => setCharactermore(e.target.value)}
+                    options={Object.keys(selectCharacters)}>
+                  </Select>
+                </li>
+                <li><Link to={'/charactermore?character=' + charactermore}> <HeaderButton name="Вернуть" /></Link> </li>
+              </ul>
+            </li>
+            <li className='OperationItem'>
+              <ul className='NeededOperations'>
+                <li>
+                  <Select
+                    name="Количество драконов, тип которых меньше:"
+                    enter="Введите тип"
+                    value={typeless} onChange={(e) => setTypeless(e.target.value)}
+                    options={Object.keys(selectTypes)}>
+                  </Select>
+                </li>
+                <li><Link to={'/typeless?type=' + typeless}> <HeaderButton name="Вернуть" /> </Link></li>
+              </ul>
+            </li>
+          </ul>
+
+        </footer>
       </div>
     );
   }
